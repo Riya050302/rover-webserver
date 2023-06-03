f57c3e117d461c6455c9db673430f00ceeffbfe3
Riya050302 <riya.chard@hotmail.co.uk>
2023-06-03 13:01:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const app = express();
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse incoming JSON data
 
+//new variables 
+let current_coordinates = { };
+let coordinates = []; // Empty array for coordinates
+let walls_plotted = []; 
+let NewWall = false;
+let wall_detection = false;
+let maze_complete = false;
+let roverCoordinates = null;
+let roverWallDetection = null; 
+let wallCoordinate = null;
+let constant = null;
+
+
 
 function plot(coordinates) {
   //================FOR CLIENT LAPTOP================================================
@@ -23,10 +36,10 @@ function plot(coordinates) {
   //POST REQUEST (Client Laptop)======================================================
   let mvmtClicks = []; // Array to store button click data
   app.post("/mvmtClickPost", (req, res) => {
-    const { direction } = req.body; // Extract the direction from the request body
-    console.log("Button clicked:", direction); // Log the clicked direction
+    const { manual_direction } = req.body; // Extract the direction from the request body
+    console.log("Button clicked:", manual_direction); // Log the clicked direction
 
-    mvmtClicks.push(direction); // Add the clicked direction to the buttonClicks array
+    mvmtClicks.push(manual_direction); // Add the clicked direction to the buttonClicks array
 
     setTimeout(function(){
       const removedElement = mvmtClicks.pop();
@@ -52,46 +65,79 @@ function plot(coordinates) {
     res.sendStatus(200); // Send a success status code (200)
   });
 
-  // Use the 'coordinates' parameter in your plotting code
-  // ...
+
 }
 
-// Call the plot function with the desired coordinates
-const coordinates = {
-  x: 10,
-  y: 20,
-};
+function serverAlgorithm(current_coordinates){
+  let right = "right"; // Declare and assign a value to the right variable
 
-plot(coordinates);
-
-
-//================FOR ESP32=========================================================
-//GET REQUEST (ESP32)===============================================================
-app.get("/nextDirection", (req, res) => {
-  let nextDirection = mvmtClicks; // Assuming direction is defined and initialized
-  if (modeType == "manual") {
-    res.json({ nextDirection: nextDirection });
-    console.log(nextDirection);
-  } 
-});
-
-//POST REQUEST (ESP32)==============================================================
-let roverCoordinates = null
-app.post("/roverCoordinatePost", (req, res) => {
-  const { coordinates } = req.body; // Extract the coordinates from the request body
-  console.log("Received coordinates:", coordinates); // Log the received coordinates // You can perform any necessary processing with the coordinates here
-
-  roverCoordinates = coordinates
-  
-  res.sendStatus(200); // Send a success status code (200)
-});
-
-app.get("/roverCoordinates", (req, res) => {
-  res.json({
-    roverCoordinates: roverCoordinates,
-  });
-});
+  return right; // Use the right variable in your code
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`); // Start the server and log the port it's listening on
 });
+
+while (!maze_complete) {
+
+  //================FOR ESP32=========================================================
+    //POST REQUEST (ESP32)==============================================================
+
+  app.post("/roverCoordinatePost", (req, res) => {
+    const { received_coordinates } = req.body; // Extract the coordinates from the request body
+    console.log("Received coordinates:", received_coordinates); // Log the received coordinates // You can perform any necessary processing with the coordinates here
+    roverCoordinates = received_coordinates;   
+    // Check if the maze is complete based on the received coordinates
+    // Set the mazeComplete flag accordingly
+    res.sendStatus(200); // Send a success status code (200)
+  });
+
+  
+  coordinates = coordinates.concat(current_coordinates);
+
+  app.post("/wallDetection", (req, res) => {
+    const {recieved_walldetection } = req.body; // Extract the coordinates from the request body
+    console.log("Received wall detection:", recieved_walldetection); // Log the received coordinates // You can perform any necessary processing with the coordinates here
+    roverWallDetection = recieved_walldetection;   
+    res.sendStatus(200); // Send a success status code (200)
+  });
+
+  wall_detection = roverWallDetection
+
+  wallCoordinate = current_coordinates + constant
+
+  if (wall_detection && !walls_plotted.includes(wallCoordinate)) {
+    walls_plotted = walls_plotted.concat(wallCoordinate);
+    NewWall = true;
+    //GET REQUEST (ESP32)===============================================================
+    app.get("/", (req, res) => {
+      // Send left_following along with nextDirection
+        res.json({newWall: NewWall });
+        console.log(NewWall);
+    });
+
+    plot(wallCoordinate)
+  }
+  else{
+    NewWall = false;
+    app.get("/", (req, res) => {
+      // Send left_following along with nextDirection
+        res.json({newWall: NewWall });
+        console.log(NewWall);
+    });
+
+    direction = serverAlgorithm(current_coordinates)
+
+    app.get("/", (req, res) => {
+      // Send left_following along with nextDirection
+        res.json({Direction: direction });
+        console.log(direction);
+    });
+  }
+
+}
+
+  //app.get("/roverCoordinates", (req, res) => {
+  //  res.json({
+  //    roverCoordinates: roverCoordinates,
+  //  });
