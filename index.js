68b11542b3c910c87de0a4817692702344a95d26
Riya050302 <riya.chard@hotmail.co.uk>
2023-06-04 12:26:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json()); // Parse incoming JSON data
 
 //new variables 
 let iterations = 0
-let current_coordinates = { };
+let current_coordinates;
 let coordinates = []; // Empty array for coordinates
 let walls_plotted = []; 
 let NewWall = false;
@@ -26,47 +26,47 @@ var modeType;
 
 
 function plot(coordinates, mode) {
-  //================FOR CLIENT LAPTOP================================================
-//GET REQUEST (Client Laptop)======================================================
-app.get("/numericalInput", (req, res) => {
-  var numericalInput = 70; // Replace with your desired numerical input
+    //================FOR CLIENT LAPTOP================================================
+  //GET REQUEST (Client Laptop)======================================================
+  app.get("/numericalInput", (req, res) => {
+    var numericalInput = 70; // Replace with your desired numerical input
 
-  res.json({
-    numericalInput: numericalInput,
+    res.json({
+      numericalInput: numericalInput,
+    });
   });
-});
 
-//POST REQUEST (Client Laptop)======================================================
-let mvmtClicks = []; // Array to store button click data
-app.post("/mvmtClickPost", (req, res) => {
-  const { direction } = req.body; // Extract the direction from the request body
-  console.log("Button clicked:", direction); // Log the clicked direction
+  //POST REQUEST (Client Laptop)======================================================
+  let mvmtClicks = []; // Array to store button click data
+  app.post("/mvmtClickPost", (req, res) => {
+    const { direction } = req.body; // Extract the direction from the request body
+    console.log("Button clicked:", direction); // Log the clicked direction
 
-  mvmtClicks.push(direction); // Add the clicked direction to the buttonClicks array
+    mvmtClicks.push(direction); // Add the clicked direction to the buttonClicks array
 
-  setTimeout(function(){
-    const removedElement = mvmtClicks.pop();
-    //console.log(removedElement);
-    //console.log("Delayed by 2 seconds");
-  }, 5000);
-  
-  res.sendStatus(200); // Send a success status code (200)
-});
+    setTimeout(function(){
+      const removedElement = mvmtClicks.pop();
+      //console.log(removedElement);
+      //console.log("Delayed by 2 seconds");
+    }, 5000);
 
-app.get("/mvmtClicks", (req, res) => {
-  res.json({
-    mvmtClicks: mvmtClicks, // Return the buttonClicks array as JSON response
+    res.sendStatus(200); // Send a success status code (200)
   });
-});
 
-var modeType;
-app.post("/setManualMode", (req, res) => {
-  const { mode } = req.body; // Extract the mode from the request body
+  app.get("/mvmtClicks", (req, res) => {
+    res.json({
+      mvmtClicks: mvmtClicks, // Return the buttonClicks array as JSON response
+    });
+  });
 
-  modeType = mode; // Update the manual mode flag
-  console.log("Mode:", mode);
-  res.sendStatus(200); // Send a success status code (200)
-});
+
+  app.post("/setManualMode", (req, res) => {
+    const { mode } = req.body; // Extract the mode from the request body
+
+    modeType = mode; // Update the manual mode flag
+    console.log("Mode:", mode);
+    res.sendStatus(200); // Send a success status code (200)
+  });
 
 }
 
@@ -83,26 +83,25 @@ function serverAlgorithm(current_coordinates){
   app.post("/roverCoordinatePost", (req, res) => {
     const { received_coordinates } = req.body; // Extract the coordinates from the request body
     console.log("Received coordinates:", received_coordinates); // Log the received coordinates // You can perform any necessary processing with the coordinates here
-    roverCoordinates = received_coordinates;   
+    current_coordinates = received_coordinates;   
     // Check if the maze is complete based on the received coordinates
     // Set the mazeComplete flag accordingly
+    coordinates = coordinates.concat(current_coordinates);
+    console.log("Updated coordinates:", coordinates);
+    const parsedCoordinates = received_coordinates.split(",").map(coord => parseInt(coord.trim()));
+    wallCoordinate = parsedCoordinates.map(coord => coord + constant);
     res.sendStatus(200); // Send a success status code (200)
   });
 
-  
-  coordinates = coordinates.concat(current_coordinates);
 
   app.post("/wallDetection", (req, res) => {
     const {recieved_walldetection } = req.body; // Extract the coordinates from the request body
     console.log("Received wall detection:", recieved_walldetection); // Log the received coordinates // You can perform any necessary processing with the coordinates here
-    roverWallDetection = recieved_walldetection;   
+    wall_detection = recieved_walldetection;   
     res.sendStatus(200); // Send a success status code (200)
   });
 
-  wall_detection = roverWallDetection
-
-  wallCoordinate = current_coordinates + constant
-
+  
   if (wall_detection && !walls_plotted.includes(wallCoordinate)) {
     walls_plotted = walls_plotted.concat(wallCoordinate);
     NewWall = true;
@@ -133,13 +132,11 @@ function serverAlgorithm(current_coordinates){
     });
   }
 
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`); // Start the server and log the port it's listening on
+});
 
   //app.get("/roverCoordinates", (req, res) => {
   //  res.json({
   //    roverCoordinates: roverCoordinates,
   //  });
-
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`); // Start the server and log the port it's listening on
-});
