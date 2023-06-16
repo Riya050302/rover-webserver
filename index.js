6b2aae85288a22510a9254ecfa3950002964bad2
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-06-16 15:35:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function plot() {
   app.get("/numericalInput", (req, res) => {
     // Generate random coordinates and add them to the array
     coordinates.push({ x, y }); // Add the coordinate to the array
-    console.log([x, y], ",");
+    //console.log([x, y], ",");
     //   console.log(plot);
 
     res.json({ coordinates }); // Send the coordinates array as a single response
@@ -70,7 +70,7 @@ app.post("/recalibratePost", (req, res) => {
   const { new_recalibrate } = req.body; // Extract the direction from the request body
   //console.log("Button clicked:", direction); // Log the clicked direction
   recalibrate = new_recalibrate; // Add the clicked direction to the buttonClicks array
-  //console.log("recalibrate:", recalibrate);
+  console.log("recalibrate:", recalibrate);
   res.sendStatus(200); // Send a success status code (200)
 });
 
