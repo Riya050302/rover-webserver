c0a92df8c1cb537085bb28b42d46857a96a2eefc
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-06-16 15:37:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post("/recalibratePost", (req, res) => {
   const { new_recalibrate } = req.body; // Extract the direction from the request body
   //console.log("Button clicked:", direction); // Log the clicked direction
   recalibrate = new_recalibrate; // Add the clicked direction to the buttonClicks array
-  console.log("recalibrate:", recalibrate);
+  //console.log("recalibrate:", recalibrate);
   res.sendStatus(200); // Send a success status code (200)
 });
 
@@ -84,9 +84,9 @@ app.post("/stopleftPost", (req, res) => {
   const { new_stopleft } = req.body; // Extract the direction from the request body
   //console.log("Button clicked:", direction); // Log the clicked direction
   stopleft = new_stopleft; // Add the clicked direction to the buttonClicks array
-  if (stopleft === "true") {
+  //if (stopleft === "true") {
     console.log("stop:", stopleft);
-  }
+  //}
   res.sendStatus(200); // Send a success status code (200)
 });
 
