c039913754e1448fe43e96f857f893e3d8dfab55
anish-narain <69715492+anish-narain@users.noreply.github.com>
2023-06-20 13:20:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function serverAlgorithm(received_coordinates){
   //});
 
   app.get("/nextDirectionAndRecalibrateAndStopLeft", (req, res) => {
-    console.log("Recalibrate:", recalibrate);
+   // console.log("Recalibrate:", recalibrate);
     recalibrate_var = recalibrate.pop()
     stopLeft_var = stopLeft.pop()
     res.json({ Direction: direction, Recalibrate : recalibrate, StopLeft : stopLeft , Recalibrate : recalibrate_var, StopLeft : stopLeft_var});
